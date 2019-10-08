61aea6a501bb591b444264444b675c390249a943
BorjaZarco <borjia98@gmail.com>
2019-10-08 12:12:23 +0100

    unify all

--- practica1/main.py
+++ practica1/main.py
@@ -1,34 +1,36 @@
+from adaline import Adaline
 from perceptron import Perceptron
-from utils import displayPlot
+from utils import displayError
 from sklearn import datasets
 from pandas.plotting import scatter_matrix
 import pandas as pd
 import numpy as np
 
-def own_test():
-    print("Own Test begins...")
-    x = np.array([[0,3],[3,0],[0,5],[5,3],[0,0],[6,4],[4,6],[2,2],[1,1],[6,6],[5,5],[7,2],[4,6.5],[4,2.5], [4.8,0.25]])
-    y = np.array([0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0])
-    perceptron = Perceptron(eta=0.0015, epochs=500)
-    perceptron.fit(x, y, showProgress=100)
+def own_test_basic(x, y):
+    print("Own Test begins... [Basic]")
+    perceptron = Perceptron(eta=0.015, epochs=250)
+    perceptron.fit(x, y, showProgress=50)
     prediction = perceptron.predict(x)
-    accuracy = len([1 for predicted, expected in zip(prediction, y) if predicted == expected ])
+    accuracy = len([1 for predicted, expected in zip(prediction, y) if predicted == expected])
     print("Accuracy: ", round(accuracy*100/len(y),2), "%")
-    print(perceptron.get_weights())
+    print("Pesos Finales: ", perceptron.get_weights())
 
-# def iris_test():
-    # print("Iris Test begins...")
-    # iris = datasets.load_iris()
-    # x = iris.data[:, :2]  # we only take the first two features.
-    # y = iris.target
-    # print("xses: ",x," yses:",y)
-    # perceptron = Perceptron(eta=0.15, epochs=100)
-    # perceptron.fit(x, y, showProgress=50)
-    # prediction = perceptron.predict(x)
-    # accuracy = len([1 for predicted, expected in zip(prediction, y) if predicted == expected ])
-    # print("Accuracy: ", round(accuracy*100/len(y),2), "%")
-    # print(perceptron.get_weights())
+def own_test_adaline(x, y):
+    print("Own Test begins... [Adaline]")
+    adaline = Adaline(eta=0.001, epochs=500)
+    adaline.fit(x, y, showProgress=100)
+    prediction = adaline.predict(x)
+    print(prediction)
+    accuracy = len([1 for predicted, expected in zip(prediction, y) if predicted == expected])
+    print("Accuracy: ", round(accuracy*100/len(y),2), "%")
+    print("Pesos Finales: ", adaline.get_weights())
+    displayError(adaline)
+    # adaline.plot(x, y)
+
 
 if __name__ == "__main__":
-    own_test()
-    # iris_test()
+    x = np.array([[0,3],[3,0],[0,5],[5,3],[0,0],[6,4],[4,6],[2,2],[1,1],[6,6],[5,5],[7,2],[4,6.5],[4,2.5], [4.8,0.25]])
+    y = np.array([-1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, 1, -1])
+    # own_test_basic(x, y)
+    own_test_adaline(x, y)
+    # TODO: iris_test()
--- practica1/utils.py
+++ practica1/utils.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def displayPlot(m, n, x, y):
+    plt.figure('Adaline Figure [with data]')
     px1,px2,py1,py2 = list(),list(),list(),list()
    
     for coor,out in zip(x,y):
-        if(out==0):
+        if(out==-1):
             px1.append(coor[0])
             py1.append(coor[1])
         else:
@@ -15,9 +16,19 @@ def displayPlot(m, n, x, y):
     plt.plot(px1, py1, 'o', c='y')
     plt.plot(px2, py2, 'o', c='r')
     vecX = np.linspace(-1,8,100)
-    vecY = np.add(np.multiply(m, vecX), n)
+    vecY = np.multiply(m, vecX) + n
     plt.plot(vecX,vecY)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.grid(True)
     plt.show()
+
+def displayError(perceptron):
+    plt.figure('SSE Progress')
+    plt.plot(list(range(0,perceptron.get_epochs())), perceptron.get_errors())
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.grid(True)
+    plt.show()
+
+
